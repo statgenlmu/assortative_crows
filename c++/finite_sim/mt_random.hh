4d002975d7ca985ebc0ef4858e0ef235319a1714
Dirk Metzler <metzler@bio.lmu.de>
2021-06-04 11:21:18 +0200

    added copyleft info to source code files

--- c++/finite_sim/mt_random.hh
+++ c++/finite_sim/mt_random.hh
@@ -1,3 +1,24 @@
+/*
+ * hybridzone_simulator is a program in the project assortative_crows
+ *
+ * Copyright (C) 2021 Dirk Metzler, http://evol.bio.lmu.de/_statgen/
+ *
+ * This file is part of hybridzone_simulator.
+ *
+ * hybridzone_simulator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <random>
 
 extern std::mt19937 random_generator;
